7da1468e8284dcea1a4ebaf254bde8f42e16588c
NicolleF <nicolleformigari@gmail.com>
2022-05-19 17:24:01 -0300

    aulamaterialize1

---
+++ materialize/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {fullWidth: true, duration: 200, autoplay: true});
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
